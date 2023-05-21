9efdc413a991b364ba52f95296e57837ed2545be
Janani Ravishankar <janraks2015@gmail.com>
2023-05-21 21:29:55 +0530

    Committing latest

---
+++ src/app/reset-password/reset-password.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ResetPassword } from 'src/Models/Login.models';
+import { UserService } from 'src/services/user.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css']
+})
+export class ResetPasswordComponent {
+  resetPasswordform = this._fb.group({
+    token: new FormControl,
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
+  resetPassword(data: ResetPassword) {
+    
+    this._user.resetPassword(data).subscribe({
+      next: result => { console.log(result); 
+    }
+    })
+    this._route.navigate(['/login'])
+  }
+
+  constructor(private _user:UserService,
+    private _route:Router,
+    private _fb: FormBuilder){}
+}
