1029b66b14ece1f7409ece9a9820fb0750d9a046
Janani Ravishankar <janraks2015@gmail.com>
2023-05-23 23:54:43 +0530

    BookTicket#2

--- src/app/book-movie-ticket/book-movie-ticket.component.ts
+++ src/app/book-movie-ticket/book-movie-ticket.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
-
+const selectedSeats: string[] = [];
 @Component({
   selector: 'app-book-movie-ticket',
   templateUrl: './book-movie-ticket.component.html',
   styleUrls: ['./book-movie-ticket.component.css']
 })
 export class BookMovieTicketComponent {
-  name = "Angular";
-
+   bgColor : string = 'grey';
+   toggle = false;
+   status = 'Enable'; 
+  
+  
   data = {
     rowHeader : ['A','B','C','D'],
     columnHeader: ['1','2','3','4'],
@@ -16,15 +19,18 @@ export class BookMovieTicketComponent {
     //Brand: ["B1", "B2", "B3"]
   }; 
   
-onSelection()
+onSelection(column: any, row:any)
 {
-  
+  this.bgColor = 'green';
+  //this.toggle = !this.toggle;
+  //this.status = this.toggle ? 'Enable' : 'Disable';
+  selectedSeats.push(row + column);
+  console.log(selectedSeats);
 }
 
 
-
 ngOnInit() {
-  
+ 
 }
 
   
